86e28797538009d238b32848e12c139e24532002
Dandy <lavenderflyyaya@gmail.com>
2022-01-05 19:21:07 +0800

    feat new & edit hbs

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,11 @@ const router = express.Router()
 
 const home = require('./modules/home')
 const users = require('./modules/users')
+const expense = require('./modules/expense')
 
 router.use('/', home)
 router.use('/users', users)
+router.use('/expense', expense)
 
 module.exports = router
 
