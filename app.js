414df56a0124b4f0b562251e37ba8198fe622f16
Dandy <lavenderflyyaya@gmail.com>
2022-01-05 16:44:50 +0800

    feat router for login register

--- app.js
+++ app.js
@@ -2,7 +2,10 @@ const express = require('express')
 const { engine } = require('express-handlebars')
 const session = require('express-session')
 const bodyParser = require('body-parser')
+const methodOverride = require('method-override')
 
+const routes = require('./routes')
+const { route } = require('express/lib/application')
 
 const app = express()
 const PORT = 3000
@@ -11,6 +14,9 @@ app.engine('hbs', engine({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
 app.use(express.static('public'))
-app.get('/', (req, res) => res.render('index'))
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
+app.use(routes)
+
 
 app.listen(PORT, () => console.log(`App is running on http://localhost:${PORT}`))
